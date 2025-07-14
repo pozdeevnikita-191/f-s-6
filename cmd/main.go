7841b857ef376b625e352c00c95b0a858ce9d812
Nikita Pozdeev <pozdeev.nikita@yandex.ru>
2025-07-14 17:07:23 +0400

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	fmt.Println("Сервер запущен")
+	// Создаем логгер
+	logger := log.New(os.Stdout, "server: ", log.LstdFlags)
+
+	// Создаем сервер с помощью ServerFunc
+	srv := server.ServerFunc(logger)
+
+	// Запускаем сервер и обрабатываем ошибки
+	logger.Printf("Server starting on http://localhost%s\n", srv.HttpServer.Addr)
+	if err := srv.HttpServer.ListenAndServe(); err != nil {
+		logger.Fatalf("Server failed: %v", err)
+	}
 }
